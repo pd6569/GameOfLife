7d98326baab43b62888a82bdadb8f98599ad9033
Peter de Souza <peterdesouza@gmail.com>
2017-09-19 22:30:20 +0100

    function to generate next generation added

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 
 class GameOfLife {
 
-
     constructor () {
         console.log("New game of life");
 
@@ -22,24 +21,109 @@ class GameOfLife {
             '*0*00**0'
         ];
 
-        // DOM
-        this.$grid = $('#grid');
+        this.currentGeneration = this.seedGeneration;
+        this.currentGenerationNum = 0;
 
+        // DOM
+        this.$generationNum = $('#generationNum');
+        this.$grid = $('#grid');
+        this.$nextGenBtn = $('#nextGenBtn');
+
+        this.setListeners();
         this.outputGeneration()
     }
 
-    createGeneration(numGenerations){
+    setListeners(){
+        this.$nextGenBtn.on('click', () => {
+            this.getNextGen();
+        })
+    }
+
+    getNextGen(){
+        this.currentGenerationNum++;
+        let nextGen = [];
+        for (let i = 0; i < this.currentGeneration.length ; i++){
+            let newRow = [];
+            for (let j = 0; j < this.currentGeneration[i].length; j++){
+                let numNeighbours = 0;
+                let currentCell = this.currentGeneration[i][j];
+                let neighbourCell, index, firstCell, lastCell;
+
+                // Check left neighbour
+                if (j - 1 >= 0) {
+                    index = j - 1;
+                    neighbourCell = this.currentGeneration[i][index];
+                    if (neighbourCell === "*") numNeighbours++;
+                } else {
+                    firstCell = true;
+                }
+
+                // Check right neighbour
+                if (j + 1 <= 7) {
+                    index = j + 1;
+                    neighbourCell = this.currentGeneration[i][index];
+                    if (neighbourCell === "*") numNeighbours++;
+                } else {
+                    lastCell = true;
+                }
+
+                // Check row above
+                if (i - 1 >= 0){
+
+                    if (this.currentGeneration[i-1][j] === "*") numNeighbours++; // check cell directly above
+
+                    if (!lastCell) if (this.currentGeneration[i-1][j+1] === "*")numNeighbours++; // check above and right
+
+                    if (!firstCell) if (this.currentGeneration[i-1][j-1] === "*") numNeighbours++; // check above and left
+                }
+
+                // Check row below
+                if (i + 1 <= 7){
+
+                    if (this.currentGeneration[i+1][j] === "*") numNeighbours++; // check cell directly below
+
+                    if (!lastCell) if (this.currentGeneration[i+1][j+1] === "*")numNeighbours++; // check below and right
+
+                    if (!firstCell) if (this.currentGeneration[i+1][j-1] === "*") numNeighbours++; // check below and left
+                }
+
+                console.log("Checking row: " + (i+1) + " column: " + (j+1) + " num Neighbours: " + numNeighbours);
+                if (currentCell === "0"){
+                    if (numNeighbours === 3) {
+                        newRow[j] = "*";
+                    } else {
+                        newRow[j] = "0";
+                    }
+                } else if (currentCell === "*"){
+                    if (numNeighbours === 2 || numNeighbours === 3){
+                        newRow[j] = "*";
+                    } else {
+                        newRow[j] = "0";
+                    }
+                }
+            }
+            nextGen[i] = newRow;
+        }
+        console.log("Next gen: ", nextGen);
+        this.currentGeneration = nextGen;
+        this.outputGeneration();
 
     }
 
-    outputGeneration(generationArray = this.seedGeneration){
-        for (let i = 0; i < generationArray.length ; i++){
-            let row = generationArray[i];
+    outputGeneration(){
+        this.$generationNum.text(this.currentGenerationNum);
+
+        // Remove temporary cells
+        $('.temp-cell').remove();
+
+
+        for (let i = 0; i < this.currentGeneration.length ; i++){
+            let row = this.currentGeneration[i];
             let $row = this.$grid.find(`[data-row='${i+1}']`);
             for (let j = 0; j < row.length ; j++){
                 let cell = row[j];
                 if (cell == "0") cell = " ";
-                $row.append(`<td>${cell}</td>`);
+                $row.append(`<td class="temp-cell">${cell}</td>`);
             }
         }
     }
